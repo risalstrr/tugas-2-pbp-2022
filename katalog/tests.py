65d3ddc071bacc853f42ecbdda94aac61000080c
risalstr <93498117+risalstrr@users.noreply.github.com>
2022-09-11 19:37:51 +0700

    first commit

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+from katalog.models import CatalogItem
+
+# ini tdd
+
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        CatalogItem.objects.create(item_name="supreme", item_price=100, item_stock=3,
+                                   description="Oriiiii", rating=0, item_url="https://www.supremenewyork.com/")
+
+    def test_katalog_name_should_be_exist(self):
+        supreme = CatalogItem.objects.get(item_name="supreme", item_price=100, item_stock=3,
+                                          description="Oriiiii", rating=0, item_url="https://www.supremenewyork.com/")
+        self.assertEqual(supreme.item_name, "supreme")
+
+    def test_katalog_props_should_be_same(self):
+        supreme = CatalogItem.objects.get(item_name="supreme", item_price=100, item_stock=3,
+                                          description="Oriiiii", rating=0, item_url="https://www.supremenewyork.com/")
+        self.assertEqual(supreme.item_name, "supreme")
+        self.assertEqual(supreme.item_price, 100)
+        self.assertEqual(supreme.item_stock, 3)
+        self.assertEqual(supreme.description, "Oriiiii")
+        self.assertEqual(supreme.rating, 0)
+        self.assertEqual(supreme.item_url, "https://www.supremenewyork.com/")
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+
+
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_katalog,
+        'nama': 'Risa Lestari',
+        'studentId': "2106655274"
+    }
+    return render(request, "katalog.html", context)
