ccc5fad9c2da36ce531111a62c62e41ca9914040
risalstr <93498117+risalstrr@users.noreply.github.com>
2022-09-11 19:43:21 +0700

    feat: add new variabel

--- project_django/settings.py
+++ project_django/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
