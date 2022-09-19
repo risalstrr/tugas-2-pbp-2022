f3ba739d4c8adb6b4b4297617cd192038c8148e1
risalstr <93498117+risalstrr@users.noreply.github.com>
2022-09-19 14:44:46 +0700

    add unit test to mywatchlist

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,25 @@
+import datetime
 from django.test import TestCase
 
 # Create your tests here.
+from mywatchlist.models import MyWatchList
+
+
+class MywatchlistTestCase(TestCase):
+    def setUp(self):
+        MyWatchList.objects.create(watched=True, title="Stranger Things", rating=3,
+                                   release_date=datetime.date(2022, 7, 6), review="Nice!")
+
+    def test_mywatchlist_name_should_be_exist(self):
+        watchlist = MyWatchList.objects.get(watched=True, title="Stranger Things", rating=3,
+                                            release_date=datetime.date(2022, 7, 6), review="Nice!")
+        self.assertEqual(watchlist.title, "Stranger Things")
+
+    def test_mywatchlist_props_should_be_same(self):
+        watchlist = MyWatchList.objects.get(watched=True, title="Stranger Things", rating=3,
+                                            release_date=datetime.date(2022, 7, 6), review="Nice!")
+        self.assertEqual(watchlist.watched, True)
+        self.assertEqual(watchlist.title, "Stranger Things")
+        self.assertEqual(watchlist.rating, 3)
+        self.assertEqual(watchlist.release_date, datetime.date(2022, 7, 6))
+        self.assertEqual(watchlist.review, "Nice!")
