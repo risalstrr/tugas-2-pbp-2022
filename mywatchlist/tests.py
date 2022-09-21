c8b1531a846876c4272b54f9bdc2304ff50d8021
risalstr <risa.lestari2002@gmail.com>
2022-09-21 22:47:05 +0700

    update tests.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,25 +1,18 @@
-import datetime
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
-from mywatchlist.models import MyWatchList
-
-
-class MywatchlistTestCase(TestCase):
-    def setUp(self):
-        MyWatchList.objects.create(watched=True, title="Stranger Things", rating=3,
-                                   release_date=datetime.date(2022, 7, 6), review="Nice!")
-
-    def test_mywatchlist_name_should_be_exist(self):
-        watchlist = MyWatchList.objects.get(watched=True, title="Stranger Things", rating=3,
-                                            release_date=datetime.date(2022, 7, 6), review="Nice!")
-        self.assertEqual(watchlist.title, "Stranger Things")
-
-    def test_mywatchlist_props_should_be_same(self):
-        watchlist = MyWatchList.objects.get(watched=True, title="Stranger Things", rating=3,
-                                            release_date=datetime.date(2022, 7, 6), review="Nice!")
-        self.assertEqual(watchlist.watched, True)
-        self.assertEqual(watchlist.title, "Stranger Things")
-        self.assertEqual(watchlist.rating, 3)
-        self.assertEqual(watchlist.release_date, datetime.date(2022, 7, 6))
-        self.assertEqual(watchlist.review, "Nice!")
+class TestMywatchlist(TestCase):
+    def test_show_mywatchlist_html(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:show_mywatchlist"))
+        self.assertEquals(response.status_code, 200)
+    
+    def test_show_mywatchlist_xml(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:mywatchlist_xml"))
+        self.assertEquals(response.status_code, 200)
+    
+    def test_show_mywatchlist_json(self):
+        client = Client()
+        response = client.get(reverse("mywatchlist:mywatchlist_json"))
+        self.assertEquals(response.status_code, 200)
